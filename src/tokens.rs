c3b202ff2b9794d5d4f2f29736394ee8beda036a
mohamedelawakey <mohamedelawakey@gmail.com>
2026-09-07 17:12:29 +0300

    feat: implement the basic tokens of NouvaLang

---
+++ src/tokens.rs
@@ -0,0 +1,46 @@
+#[derive(Debug, PartialEq, Clone)]
+pub enum Token {
+    // Keywords
+    Let,
+    Fun,
+    If,
+    Else,
+    While,
+    Return,
+
+    // Types
+    IntType,
+    FloatType,
+    BoolType,
+    StringType,
+
+    // Literals
+    Identifier(String),
+    IntLiteral(i64),
+    FloatLiteral(f64),
+    StringLiteral(String),
+    BooleanLiteral(bool),
+    
+    // Operators
+    Plus,        // +
+    Minus,       // -
+    Asterisk,    // *
+    Slash,       // /
+    Assign,      // =
+    Equals,      // ==
+    GreaterThan, // >
+    LessThan,    // <
+
+    // Punctuation
+    Colon,       // :
+    Comma,       // ,
+    Arrow,       // ->
+    LParen,      // (
+    RParen,      // )
+    LBrace,      // {
+    RBrace,      // }
+    Comment,     // //
+    LongComment, // /* */
+
+    EOF
+}
