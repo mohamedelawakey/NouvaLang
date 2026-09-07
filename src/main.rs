da876cac974fdda5d7f07ee0d3545644c7067e75
mohamedelawakey <mohamedelawakey@gmail.com>
2026-09-07 18:55:53 +0300

    fix: add missing main.rs to fix github actions build

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+mod tokens;
+mod lexer;
+use lexer::Lexer;
+use tokens::Token;
+
+fn main() {
+    let source_code = "let x = 10 /* this comment never ends + 20";
+
+    let mut lexer = Lexer::new(source_code);
+    println!("lexer starts:");
+    
+    loop {
+        let token = lexer.next_token();
+        println!("{:?}", token);
+
+        if token == Token::EOF {
+            break;
+        }
+    }
+}
