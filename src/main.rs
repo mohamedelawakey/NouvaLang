58ddf6bca9714b4c314b6f811d962722556e8280
mohamedelawakey <mohamedelawakey@gmail.com>
2026-09-08 20:33:58 +0300

    test(parser): verify for loop and optional semicolon parsing in main

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,33 @@
 mod lexer;
 mod tokens;
+mod ast;
+mod parser;
+
 use lexer::Lexer;
-use tokens::Token;
+use parser::Parser;
 
 fn main() {
-    let source_code = "let x = 10 /* this comment never ends + 20";
-
-    let mut lexer = Lexer::new(source_code);
-    println!("lexer starts:");
-
-    loop {
-        let token = lexer.next_token();
-        println!("{:?}", token);
-
-        if token == Token::Eof {
-            break;
+    // let source_code = "let x: int = ((10 + -5) * 2) ^ 0.5 + (10 * 12 + (8 ^ 2) / 88.5) + 17";
+    // let source_code = "let is_valid: bool = (10 + 5 > 12) && (x == 20 || !false);";
+    // let source_code = "return (10 + 5 > 12) && (x == 20 || !false);";
+    let source_code = "
+        for i in 0 to 10 {
+            let x: int = i * 2;
         }
+    ";
+
+    let lexer = Lexer::new(source_code);
+    let mut parser = Parser::new(lexer);
+    
+    let program = parser.parse_program();
+
+    if !parser.errors.is_empty() {
+        println!("Syntax Errors Found:");
+        for error in parser.errors {
+            println!("  - {}", error);
+        }
+    } else {
+        println!("AST Tree:");
+        println!("{:#?}", program);
     }
 }
